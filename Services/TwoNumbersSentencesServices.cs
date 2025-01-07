d20a1b954cf10a1cac1131b19d232a9fa72bfe94
bhenry3145 <bhenry3145@gmail.com>
2025-01-07 11:51:05 -0800

    added controllers and services

---
+++ Services/TwoNumbersSentencesServices.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoToFour.Services
+{
+    public class TwoNumbersSentencesServices
+    {
+        
+    }
+}
